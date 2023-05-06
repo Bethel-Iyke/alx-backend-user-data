add4b66d4378abf91865047cdd166ba95f9756dc
Bethek-Iyke <talk2bethel017@gmail.com>
2023-05-07 00:32:34 +0100

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,18 +6,23 @@ from typing import List, TypeVar
 
 
 class Auth():
-    """the class auth"""
+    """Manage the API authentication."""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """the method require auth"""
-        if path is None:
+        """Return a boolean."""
+        if not path or not excluded_paths:
             return True
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
+
         for excluded_path in excluded_paths:
-            if path.rstrip('/').startswith(excluded_path.rstrip('/')):
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path\
+                    or path.startswith(excluded_path + '/')\
+                    or (excluded_path.endswith('/')
+                        and path.startswith(excluded_path[:-1])):
                 return False
-            return True
+        return True
 
     def authorization_header(self, request=None) -> str:
         """the method authorization header"""
