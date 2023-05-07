a7bc0d8ce191c6b28bf9fad81f3a7d3df469207b
Bethek-Iyke <talk2bethel017@gmail.com>
2023-05-07 02:35:57 +0100

    session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Creates the class Auth that has different methods"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Manage the API authentication."""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Return a boolean."""
+        if not path or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path\
+                    or path.startswith(excluded_path + '/')\
+                    or (excluded_path.endswith('/')
+                        and path.startswith(excluded_path[:-1])):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """the method authorization header"""
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """the  method current user"""
+        return None
+
+    def session_cookie(self, request=None):
+        """Returns none if cookies is none """
+        if request is None:
+            return None
+        session_id = getenv('SESSION_NAME')
+        return request.cookies.get(session_id)
